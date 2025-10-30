71b5aaede37a65d244408f006921273fad74bf04
semzz <semzzcs@gmail.com>
2025-10-30 19:36:19 +0300

    feat: Добавлен обработчик ошибок

---
+++ backend/order-service/src/main/java/ru/semstore/orderservice/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package ru.semstore.orderservice.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ErrorResponse {
+    private String message;
+
+    @JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    private LocalDateTime date;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.date = LocalDateTime.now();
+    }
+}
