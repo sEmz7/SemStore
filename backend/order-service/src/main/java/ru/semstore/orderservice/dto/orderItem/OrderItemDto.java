0bc125a955c3f23b147e6c996c2657c36758f04a
semzz <semzzcs@gmail.com>
2025-12-17 21:37:28 +0300

    docs: Описаны схемы DTO

--- backend/order-service/src/main/java/ru/semstore/orderservice/dto/orderItem/OrderItemDto.java
+++ backend/order-service/src/main/java/ru/semstore/orderservice/dto/orderItem/OrderItemDto.java
@@ -1,20 +1,26 @@
 package ru.semstore.orderservice.dto.orderItem;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Schema(description = "DTO для просмотра товара в заказе")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderItemDto {
+    @Schema(description = "ID товара", example = "5526ff00-fe2c-4191-9d35-6d5acf537869")
     private UUID id;
 
+    @Schema(description = "Ссылка на товар", example = "https://dw4.co/t/A/1tpEiZDfq")
     private String link;
 
+    @Schema(description = "Размер товара", example = "42.5")
     private String size;
 
+    @Schema(description = "Конфигурация товара", example = "черные")
     private String configuration;
 }
