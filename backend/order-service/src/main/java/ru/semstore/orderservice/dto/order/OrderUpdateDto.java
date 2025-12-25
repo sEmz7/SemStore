e4124b878792c58a4bcd1664487b2c7fd2296bf8
semzz <semzzcs@gmail.com>
2025-12-25 20:55:28 +0300

    docs: order name

--- backend/order-service/src/main/java/ru/semstore/orderservice/dto/order/OrderUpdateDto.java
+++ backend/order-service/src/main/java/ru/semstore/orderservice/dto/order/OrderUpdateDto.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 @Schema(description = "DTO для обновления заказа")
 public record OrderUpdateDto(
+        @Schema(description = "Название заказа", example = "my order", minLength = 2, maxLength = 255)
         @Size(min = 2, max = 255)
         String name,
         @Schema(description = "ID адреса", example = "5526ff00-fe2c-4191-9d35-6d5acf537869")
