f84dc890e5461b13026b57ddcfe505d8b85df564
semzz <semzzcs@gmail.com>
2025-12-25 20:41:18 +0300

    feat: Название заказа fix: Обновление заказа

--- backend/order-service/src/main/java/ru/semstore/orderservice/mapper/OrderMapper.java
+++ backend/order-service/src/main/java/ru/semstore/orderservice/mapper/OrderMapper.java
@@ -2,17 +2,23 @@
 package ru.semstore.orderservice.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import ru.semstore.orderservice.dto.order.OrderCreateDto;
 import ru.semstore.orderservice.dto.order.OrderFullDto;
 import ru.semstore.orderservice.dto.order.OrderShortDto;
+import ru.semstore.orderservice.dto.order.OrderUpdateDto;
 import ru.semstore.orderservice.model.Order;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = OrderItemMapper.class)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = OrderItemMapper.class,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface OrderMapper {
     Order toEntity(OrderCreateDto dto);
 
     OrderShortDto toShortDto(Order entity);
 
     OrderFullDto toFullDto(Order entity);
+
+    void update(@MappingTarget Order order, OrderUpdateDto dto);
 }
