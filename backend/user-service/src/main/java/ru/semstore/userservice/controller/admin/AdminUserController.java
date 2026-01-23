4cd5b4ea0aabbb445784b99e08223ff369c70650
semzz <semzzcs@gmail.com>
2026-01-23 14:18:27 +0300

    docs: user-service swagger docs

--- backend/user-service/src/main/java/ru/semstore/userservice/controller/admin/AdminUserController.java
+++ backend/user-service/src/main/java/ru/semstore/userservice/controller/admin/AdminUserController.java
@@ -1,19 +1,30 @@
 package ru.semstore.userservice.controller.admin;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.semstore.userservice.dto.page.PageResponse;
 import ru.semstore.userservice.dto.user.UserDtoWithRole;
 import ru.semstore.userservice.dto.user.UserRoleUpdateDto;
+import ru.semstore.userservice.exception.ErrorResponse;
 import ru.semstore.userservice.service.UserService;
 
 import java.util.UUID;
 
+@Tag(name = "admin: Пользователи", description = "API для операций с пользователями для админа")
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin/users")
@@ -22,21 +33,72 @@ import java.util.UUID;
 public class AdminUserController {
     private final UserService userService;
 
+    @Operation(summary = "Получить пользователя по ID", description = "Возвращает пользователя по указанному ID",
+            responses = {@ApiResponse(responseCode = "200", description = "(OK) Пользователь возвращен",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = UserDtoWithRole.class))),
+                    @ApiResponse(responseCode = "400", description = "(BAD REQUEST) Невалидные данные запроса",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "(UNAUTHORIZED) Невалидный JWT токен",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "403", description = "(FORBIDDEN) Доступ запрещен",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "(NOT FOUND) Пользователь не найден",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class)))
+            })
     @GetMapping("/{userId}")
     public UserDtoWithRole getUserById(@PathVariable("userId") UUID userId) {
         return userService.getUserByIdWithRole(userId);
     }
 
+    @Operation(summary = "Получить всех пользователей", description = "Возвращает всех пользователей с пагинацией",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "(OK) Пользователи возвращены",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = UserDtoWithRole.class)))),
+                    @ApiResponse(responseCode = "400", description = "(BAD REQUEST) Невалидные данные запроса",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "(UNAUTHORIZED) Невалидный JWT токен",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "403", description = "(FORBIDDEN) Доступ запрещен",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class)))
+            })
     @GetMapping
     public PageResponse<UserDtoWithRole> getAllUsersWithRole(
-            @PositiveOrZero @RequestParam(name ="page", defaultValue = "0") int page,
+            @PositiveOrZero @RequestParam(name = "page", defaultValue = "0") int page,
             @Positive @RequestParam(name = "size", defaultValue = "10") int size) {
         return userService.getAllUsersWithRole(page, size);
     }
 
+    @Operation(summary = "Обновить роль пользователя по ID",
+            description = "Обновляет роль пользователя по ID и возвращает его",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "(OK) Пользователь обновлен",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = UserDtoWithRole.class))),
+                    @ApiResponse(responseCode = "400", description = "(BAD REQUEST) Невалидные данные запроса",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "(UNAUTHORIZED) Невалидный JWT токен",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "403", description = "(FORBIDDEN) Доступ запрещен",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "(NOT FOUND) Пользователь не найден",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponse.class)))
+            })
     @PatchMapping("/{userId}")
     public UserDtoWithRole updateUserRole(@PathVariable("userId") UUID userId,
-                                             @Valid @RequestBody UserRoleUpdateDto dto) {
+                                          @Valid @RequestBody UserRoleUpdateDto dto) {
         return userService.updateUserRole(userId, dto);
     }
 }
--- backend/user-service/src/main/java/ru/semstore/userservice/dto/user/UserDtoWithRole.java
+++ backend/user-service/src/main/java/ru/semstore/userservice/dto/user/UserDtoWithRole.java
@@ -1,11 +1,18 @@
 package ru.semstore.userservice.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import ru.semstore.userservice.model.UserRole;
 
 import java.util.UUID;
 
+@Schema(description = "DTO для просмотра пользователя с его ролью")
 public record UserDtoWithRole(
+        @Schema(description = "ID пользователя", example = "f47ac10b-58cc-4372-a567-0e02b2c3d479")
         UUID id,
+
+        @Schema(description = "Email пользователя", example = "example@test.com")
         String email,
+
+        @Schema(description = "Роль пользователя", example = "ROLE_USER")
         UserRole role) {
 }
