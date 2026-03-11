99de00366ab3f5426e60075bc963296b13092aae
semzz <semzzcs@gmail.com>
2026-03-11 15:10:30 +0300

    feat: Уведомление о созданном заказе

--- backend/notification-service/src/main/java/ru/semstore/notificationservice/kafka/KafkaConsumer.java
+++ backend/notification-service/src/main/java/ru/semstore/notificationservice/kafka/KafkaConsumer.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.semstore.common.dto.OrderCheckedEvent;
 import ru.semstore.notificationservice.service.EmailService;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -13,7 +16,23 @@ public class KafkaConsumer {
     private final EmailService emailService;
 
     @KafkaListener(topics = "order-checked")
-    public void orderCheckedNotification() {
+    public void orderCheckedNotification(OrderCheckedEvent event) {
+        log.debug("Received event, orderId={}", event.getOrderId());
+        if (!event.getValid()) {
+            log.debug("Order {} is invalid, skip email", event.getOrderId());
+            return;
+        }
+        if (event.getUserEmail() == null || event.getUserEmail().isBlank()) {
+            log.warn("Email is empty for order {}", event.getOrderId());
+            return;
+        }
 
+        String subject = "Ваш заказ №" + event.getOrderId() + " создан";
+        emailService.sendHtmlMessage(
+                event.getUserEmail(),
+                subject,
+                "order-created",
+                Map.of("orderId", event.getOrderId())
+        );
     }
 }
